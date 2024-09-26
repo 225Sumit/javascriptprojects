9257dd1d7c3c578c0fc4923009ac560b5a76153f
225sumit <225sumitgupta@gmail.com>
2024-09-26 13:27:31 +0530

    color change page

---
+++ project1/script.js
@@ -0,0 +1,20 @@
+
+ var index=0;
+
+ function changeColor(){
+    var color =["red",'green','black']
+    document.getElementsByTagName('body')[0]
+    .style.background= color[index++];
+    if(index > color.lenght-1 )
+        index = 0; 
+}
+
+   
+    
+  
+        
+        
+   
+
+   
+    
