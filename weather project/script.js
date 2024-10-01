10b5a30143f7c7506958daffa54b44a40f24e675
225sumit <225sumitgupta@gmail.com>
2024-10-01 11:41:44 +0530

    add whater project

--- weather project/script.js
+++ weather project/script.js
@@ -1,20 +1,34 @@
-// const cityname =document.querySelector("#input")
-// const btn=document.querySelector("#sub")
+const cityname = document.querySelector("#input");
+const btn = document.querySelector("#sub");
+const temp = document.querySelector("#temp");
+const search = document.querySelector("#input");
 
-// const apikey = '863242cfb2b1d357e6093d9a4df19a4b'
-const apiurl ='https://api.publicapis.org/entries';
-async function checkweather() {
-    const response = await fetch(apiurl );
-    var data = await response.json();
-    console.log(data);
-    
+const apikey = "09b9bbd84a58f8c6cc81616fbf165811";
+const apiurl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+async function checkweather(city) {
+  const response = await fetch(apiurl + city + `&appid=${apikey}`);
+  var data = await response.json();
+//   console.log(data);
+//   console.log(data.name);
+//   console.log(Math.round(data.main.temp));
+  Cityname.innerHTML = data.name;
+  temp.innerHTML = Math.round(data.main.temp) ;
 }
-checkweather();
 
-// console.log(cityname.value);
-// btn.addEventListener("click",function(){
+
+
+btn.addEventListener("click", function () {
+ 
+    checkweather(search.value);
+
+
+});
+search.addEventListener("keydown", function (e) {
+    if(e.key=="Enter")
+    {
+        console.log("enter");
+    checkweather(search.value);
+    }
+       
     
-    
-//     Cityname.innerHTML=input.value;
-//     console.log(input.value);
-// })
+  });
